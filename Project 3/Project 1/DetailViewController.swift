650aff7e1397667c7ab2e596ae7946e27dbce8d3
Ahmet <aegoksu@gmail.com>
2022-10-28 16:31:57 +0300

    Project 3 created

---
+++ Project 3/Project 1/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  Project 1
+//
+//  Created by AHMET on 25.09.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var selectedPictureNumber : Int!
+    var totalPictures : Int!
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    //DetailViewController'a yüklenecek görüntünün adını tutacak bir özellik oluşturmak.Bu özellik bir dize olacak ve yüklenecek görüntünün adı – ancak isteğe bağlı bir dize olması gerekiyor
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "\(selectedPictureNumber!) of \(totalPictures!)"
+        navigationItem.largeTitleDisplayMode = .never
+        // View Controllerdan Miras aldığından dolayı büyük göründü, normal görünmesini sağlamak için bu kodu yazıyoruz.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+        // Bu doğru olarak ayarlandığında, kullanıcı gezinme çubuğunu gizlemek için geçerli görünüm denetleyicisinde herhangi bir yere dokunabilir ve ardından göstermek için tekrar dokunabilir.
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+        // gezinme çubuğunu sadece burada gizlemek için
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else
+        {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+}
